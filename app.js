23f2e35342154bcdc7f591109a2624c09fbbe2c2
Rohit Ramchandra Jadhav <54153210+Rj9922@users.noreply.github.com>
2021-12-20 19:26:40 +0530

    last commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const priceInput = document.querySelector("[name=price]");
 const quantityInput = document.querySelector("[name=quantity]");
 const total = document.querySelector(".total");
+const label = document.querySelector(".badge");
 
 // create our function
 function calculatePieCost() {
@@ -11,7 +12,13 @@ function calculatePieCost() {
   total.innerText = `$` + cost.toFixed(2);
 }
 
+function updateQtyLabel() {
+  const quantity = quantityInput.value;
+  label.innerText = quantity;
+}
+
 calculatePieCost();
 // create the event listners
 priceInput.addEventListener("input", calculatePieCost);
 quantityInput.addEventListener("input", calculatePieCost);
+quantityInput.addEventListener("input", updateQtyLabel);
